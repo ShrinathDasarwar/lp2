efd93cbe8d20a3b776dde06523de4fe9b4e8bdc6
ShrinathDasarwar <157683737+ShrinathDasarwar@users.noreply.github.com>
2025-05-05 11:51:52 +0530

    Update shri2.py

--- shri2.py
+++ shri2.py
@@ -1,1 +1,8 @@
-print("Hello")
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = 5
+print(f"The factorial of {number} is {factorial(number)}")
